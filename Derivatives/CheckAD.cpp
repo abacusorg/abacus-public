555d8e87b30e38ed4528fa60f66e8943fd7749f8
Daniel Eisenstein <deisenstein@cfa.harvard.edu>
2013-02-04 15:05:17 -0500

    Adding first code for Derivatives and Multipoles.

---
+++ Derivatives/CheckAD.cpp
@@ -0,0 +1,361 @@
+#include "../include/header.cpp"
+#include "quad_double.cpp"
+#include "../include/threevector.hh"
+using namespace std;
+
+typedef ThreeVector<qd_real> qd_real3;
+
+
+#define N 4
+
+
+#define ODIM (32+1)
+
+#define FORALLREPLICAS(n,r) \
+            for(n.x=-r;n.x<=r;n.x++) \
+                for(n.y=-r;n.y<=r;n.y++) \
+                    for(n.z=-r;n.z<=r;n.z++) 
+
+
+qd_real fact(int n) {
+    assert(n>=0);
+    qd_real f=1;
+    for(int i=2;i<=n;i++) f*=i;
+    return f;
+}
+
+// (-1)^n as an integer
+int powofmone(int n) {
+    int s=1;
+    for(int i=0; i<n; i++) s *= -1;
+    return s;
+}
+
+// Read in derivatives from file
+// (Made with MakeDerivatives.cpp)
+qd_real AnalyticDerivatives[ODIM][ODIM][ODIM];
+void MakeAnalyticDerivatives(int maxorder, int innerradius) {
+    assert(maxorder < ODIM);
+
+    for(int a=0;a<ODIM;a++) 
+        for(int b=0;b<ODIM;b++) 
+            for(int c=0;c<ODIM;c++) 
+                AnalyticDerivatives[a][b][c] = 0;
+
+    int tmporder, tmpinneradius;
+    char fname[1024];
+    sprintf(fname,"AD32_%03d.dat",innerradius);
+    printf("reading derivatives from %s\n", fname);
+    ifstream fin(fname,ios::in);
+    if(!fin.is_open()) {
+        fprintf(stderr,"couldn't open \"%s\"\n",fname);
+        exit(1);
+    }
+    fin >> tmporder >> tmpinneradius;
+    if(tmporder<maxorder || tmpinneradius!=innerradius) {
+        fprintf(stderr,"file data does not agree with arguments\n");
+        exit(1);
+    }
+    char num[1024];
+    for(int a=0; a<=32; a+=2) {
+        for(int b=a; b<=32-a; b+=2) {
+            int atmp, btmp;
+            fin >> atmp >> btmp >> num;
+//                   printf("READINGING %3d %3d  %s\n", atmp, btmp, num);
+            assert(a==atmp && b==btmp);
+            AnalyticDerivatives[a][b][0] = qd_real(num);
+            AnalyticDerivatives[b][a][0] = qd_real(num);
+        }
+    }
+    fin.close();
+
+    for(int c=2;c<=32;c+=2) {
+        for(int a=0;a<=32-c;a++) {        
+            for(int b=0;b<=32-a-c;b++) {
+                AnalyticDerivatives[a][b][c] = 
+                    -AnalyticDerivatives[a+2][b][c-2]
+                    -AnalyticDerivatives[a][b+2][c-2];
+            }
+        }
+    }
+
+    for(int c=2;c<=32;c+=2) {
+        for(int a=0;a<=32-c;a++) {
+            for(int b=0;b<=32-a-c;b++) {
+                if( (AnalyticDerivatives[a][b][c]  > qd_real("0")) || (AnalyticDerivatives[a][b][c] < qd_real("0")) ) { 
+                    // printf("%d %d %d   ", a,b,c);
+                    // cout << AnalyticDerivatives[a][b][c] << endl;
+                }
+            }
+        }
+    }
+
+
+    printf("Done making derivatives \n");
+}
+
+qd_real PI = qd_real::_pi;
+qd_real SPI = sqrt(qd_real::_pi);
+qd_real PI2 = 2*qd_real::_pi;
+
+// Ewald must go out to a radius of 8 for 63 digits
+qd_real3 ewaldacc( qd_real3 ri,  qd_real3 rj) {
+    qd_real r2,nr,h2;
+    qd_real3 r,dr;
+    integer3 n,h;
+    qd_real3 ewald;
+    ewald.zero();
+    r=ri-rj;
+    FORALLREPLICAS(n,8) {
+        dr=r-n;
+        r2 = dr.norm2(); nr = sqrt(r2);
+        ewald += (erfc(SPI*nr) + 2*nr * exp(-PI*r2))/(r2*nr) * dr;
+    }
+    FORALLREPLICAS(h,8) {
+        h2 = h.x*h.x + h.y*h.y + h.z*h.z;
+        if(h2 > qd_real("0") ) ewald  += 2/h2 * exp(-PI*h2) * sin(PI2*h.dot(r)) * h ;
+    }
+    return ewald;
+}
+
+qd_real3 pos[N];
+qd_real mass[N];
+
+qd_real M[ODIM][ODIM][ODIM];
+
+void MakePoints(void) {
+#if 0
+    srand48(234234234);
+
+    for(int i=0;i<N;i++) {
+        pos[i].x = to_qd_real(drand48()-0.5);
+        pos[i].y = to_qd_real(drand48()-0.5);
+        pos[i].z = to_qd_real(drand48()-0.5);
+        mass[i] = to_qd_real(drand48());
+    }
+#else
+    assert(N==4);
+    pos[0].x =  qd_real(1)/3;
+    pos[0].y =  qd_real(1)/3;
+    pos[0].z =  qd_real(1)/3;
+    mass[0] = 1;
+    pos[1].x = -qd_real(1)/3;
+    pos[1].y =  qd_real(1)/3;
+    pos[1].z =  qd_real(1)/3;
+    mass[1] = 1;
+    pos[2].x = -qd_real(1)/3;
+    pos[2].y = -qd_real(1)/3;
+    pos[2].z =  qd_real(1)/3;
+    mass[2] = 1;
+    pos[3].x = -qd_real(1)/3;
+    pos[3].y = -qd_real(1)/3;
+    pos[3].z = -qd_real(1)/3;
+    mass[3] = 1;
+#endif
+
+}
+
+void MakeMultipolesAcc(int order) {
+    
+    for(int i=0;i<=order;i++) 
+        for(int j=0;j<=order-i;j++) 
+            for(int k=0;k<=order-i-j;k++) 
+                M[i][j][k] = 0;
+
+    for(int i=0;i<=order;i++) 
+        for(int j=0;j<=order-i;j++) 
+            for(int k=0;k<=order-i-j;k++) 
+                for(int p=0;p<N;p++) 
+                    if(p!=100) {
+                        qd_real3 q;
+                        q=pos[p];
+                        qd_real term = mass[p] * pow(q.x,i) * pow(q.y,j) * pow(q.z,k);
+                        M[i][j][k] += term;
+                    }
+
+    for(int i=0;i<=order;i++) 
+        for(int j=0;j<=order-i;j++) 
+            for(int k=0;k<=order-i-j;k++) 
+                M[i][j][k] /= (fact(i) * fact(j) * fact(k)); 
+
+}
+
+
+
+#define FORALL_RADII(n,r)  for(n.x=-r;n.x<=r;n.x++) for(n.y=-r;n.y<=r;n.y++) for(n.z=-r;n.z<=r;n.z++) 
+
+#define FOR(a,b,c) for(a=b;a<=c;a++)
+#define FORALL_COMPLETE_MULTIPOLES_BOUND(a,b,c,u)  FOR(a,0,u) FOR(b,0,u-a) FOR(c,0,u-a-b)
+
+
+
+
+
+qd_real T[ODIM][ODIM][ODIM];
+
+void MakeTaylor(int order) {
+
+    for(int k=0;k<=1;k++)     
+        for(int i=0;i<=order-k;i++) 
+            for(int j=0;j<=order-k-i;j++)  {
+                qd_real s = 0;
+                for(int a=0;a<=order-(i+j+k);a++) 
+                    for(int b=0;b<=order-(i+j+k)-a;b++) 
+                        for(int c=0;c<=order-(i+j+k)-a-b;c++) {
+                            s += M[a][b][c] *
+                                 AnalyticDerivatives[a+i][b+j][c+k];
+                        }
+                 T[i][j][k] = s;
+            }
+
+    for(int k=2;k<=order;k++)  
+        for(int i=0;i<=order-k;i++)
+            for(int j=0;j<=order-k-i;j++)  
+                T[i][j][k] = -T[i+2][j][k-2] -T[i][j+2][k-2];
+
+    for(int i=0;i<=order;i++)
+        for(int j=0;j<=order-i;j++)  
+            for(int k=0;k<=order-i-j;k++) 
+                T[i][j][k] *= qd_real(powofmone(i+j+k)) /
+                    (fact(i)*fact(j)*fact(k));
+}
+
+qd_real3 EvaluateTaylorAcc(int order, qd_real3 rp) {
+    qd_real fi,fij,fijk;
+
+    qd_real3 a;
+
+    a.zero();
+
+    fi = 1;
+    for(int i=0;i<=order-1;i++) {
+        fij = fi;
+        for(int j=0;j<=order-1-i;j++) {
+            fijk = fij;
+            for(int k=0;k<=order-1-i-j;k++) {
+                a.x += qd_real(i+1) * T[i+1][j][k] * fijk;
+                a.y += qd_real(j+1) * T[i][j+1][k] * fijk;
+                a.z += qd_real(k+1) * T[i][j][k+1] * fijk;
+                fijk *= rp.z;
+            }
+            fij *= rp.y;
+        }
+        fi *= rp.x;
+    }
+
+    return a;
+}
+
+
+qd_real3 acc( qd_real3 r) {
+    qd_real ir = 1/r.norm();
+    qd_real ir3 = ir*ir*ir;
+    qd_real3 a;
+    a=ir3*r;
+    return a;
+}
+
+
+qd_real3 latticeacc(int radius,   qd_real3 r) {
+    integer3 n;
+    qd_real3 a;
+    
+    a.zero();
+    FORALL_RADII(n,radius) {
+        if(n.norm2() <=radius*radius) {
+            qd_real3 x;
+            x=r+n;
+            a+=acc(x);
+        }
+    }
+    return a;
+}
+
+void TestAcc(int order, int innerradius) {
+    MakeAnalyticDerivatives(order, innerradius);
+    MakePoints();
+
+    
+
+    printf("Making Multipoles \n");
+    MakeMultipolesAcc(order);
+    printf("Done Making Multipoles \n");
+
+    printf("Making Taylor \n");
+    MakeTaylor(order);
+    printf("End Making Taylor\n");
+
+    
+    for(int j=0;j<N;j++) { 
+
+        qd_real3 ewald;
+        ewald.zero();
+        for(int i=0;i<N;i++) {
+            if(i!=j) {
+                ewald+=mass[i]*ewaldacc(pos[i],pos[j]);
+            }
+        }
+
+        qd_real3 l;
+        l.zero();
+        for(int i=0;i<N;i++) {
+            if(i!=j) {
+                qd_real3 r;
+                r=pos[i]-pos[j];
+                l+=mass[i]*latticeacc(innerradius,r);
+            }
+        }
+        
+        qd_real3 red;
+        red.zero();
+        
+        for(int i=0;i<N;i++) {
+            if(i!=j) {
+                qd_real3 r;
+                r=pos[i]-pos[j];
+                red+= qd_real(4)/qd_real(3)*PI*mass[i]*r;     
+            }
+        }
+        
+        qd_real3 t;
+        t = EvaluateTaylorAcc(order, pos[j]);
+        
+        qd_real3 ans;
+        ans=l-red+t;
+        qd_real3 err;
+        err=ans-ewald;
+
+        qd_real3 re;
+        re=err/ewald.norm();
+
+#if 0        
+        printf("ans: \n");
+        cout << "  " << ans.x << "  " << ans.y << "  " << ans.z << endl;
+        printf("ewald: \n");
+        cout << "  " << ewald.x << "  " << ewald.y << "  " << ewald.z << endl;
+        printf("rel err: \n");
+        cout << "  " << re.x << "  " << re.y << "  " << re.z << endl;
+        printf("\n");
+#else
+        printf("relerr = % e  % e  % e\n",
+               (double)re.x, (double)re.y, (double)re.z);
+#endif
+    }
+    printf("\n");
+}
+
+
+int main(void) {
+   unsigned int old_cw;
+    fpu_fix_start(&old_cw);
+
+    cout.precision(qd_real::_ndigits);
+//     int nd = qd_real::_ndigits + 8;
+
+    for(int innerradius=8; innerradius<=64; innerradius*=2) {
+        printf("Acc:\n");
+        TestAcc(32,innerradius);
+    }
+
+    fpu_fix_end(&old_cw);
+}
---
+++ Derivatives/GenerateAD.cpp
@@ -0,0 +1,176 @@
+#include "../include/header.cpp"
+#include "quad_double.cpp"
+#include "../include/threevector.hh"
+#include <omp.h>
+
+
+using namespace std;
+
+typedef ThreeVector<qd_real> qd_real3;
+
+#define ODIM (32+1)
+
+int _rmap[33*33];
+int TL;
+
+#define rmap(i,j,k) _rmap[ (i)*33 + j ]
+
+void makeEvenrmap(void) {
+    int m = 0;
+    for(int i=0;i<=32;i++)
+        for(int j=0;j<=32;j++) {
+            _rmap[ i*33 + j ] = m;
+            m++;
+        }
+
+    TL = m;
+}
+
+// read in the analytic far derivatives
+qd_real AD[ODIM][ODIM];
+void GetAD(int maxorder) {
+    assert(maxorder<ODIM);
+    
+    for(int a=0;a<ODIM;a+=2) for(int b=0;b<ODIM;b+=2) AD[a][b] = 0;
+
+    FILE *fp = fopen("Order32","r");
+    assert(fp!=NULL);
+    char num[256];
+    for(int i=0; i<79; i++) {
+        int a, b;
+        int dummy = fscanf(fp,"%d %d %s",&a, &b, num);
+        AD[a][b] = qd_real(num);
+    }
+    fclose(fp);
+
+    for(int a=0;a<ODIM;a+=2) 
+        for(int b=0;b<a;b+=2) 
+            if( a+b>2 && a+b<ODIM) 
+                AD[a][b] = AD[b][a];
+}
+
+void QD_ab0(qd_real3 p, qd_real *d) {
+    int i,j;
+    qd_real x,y,z,Rm1,Rm2,Rm3,Rm5,Rm7;
+
+    int order = 32;
+
+    x = p.x;
+    y = p.y;
+    z = p.z;
+
+    Rm1 = "1";
+    Rm1 = Rm1/sqrt(x*x+y*y+z*z);
+    Rm2 = Rm1 * Rm1;
+    Rm3 = Rm1 * Rm2;
+    Rm5 = Rm3 * Rm2;
+
+    d[rmap(0,0,0)] = Rm1;
+    if(order>=1) {
+        d[rmap(1,0,0)] = -x*Rm3;
+        d[rmap(0,1,0)] = -y*Rm3;
+    }
+    if(order>=2) {
+        d[rmap(1,1,0)] = 3*x*y*Rm5;
+    }
+
+    for(i=2;i<=order;i++) d[rmap(i,0,0)] = -Rm2*( (2*(i)-1)*x*d[rmap(i-1,0,0)] + (i-1)*(i-1)*d[rmap(i-2,0,0)] );
+    for(i=2;i<=order-1;i++) d[rmap(i,1,0)] = -Rm2*( (2*(i)-1)*x*d[rmap(i-1,1,0)] + (i-1)*(i-1)*d[rmap(i-2,1,0)] + 2*y*d[rmap(i,0,0)] );
+
+    for(j=2;j<=order;j++) d[rmap(0,j,0)] = -Rm2*( (2*(j)-1)*y*d[rmap(0,j-1,0)] + (j-1)*(j-1)*d[rmap(0,j-2,0)] );
+    for(j=2;j<=order-1;j++) d[rmap(1,j,0)] = -Rm2*( 2*x*d[rmap(0,j,0)] + (2*(j)-1)*y*d[rmap(1,j-1,0)] + (j-1)*(j-1)*d[rmap(1,j-2,0)] );
+
+    for(i=2;i<=order;i++)
+        for(j=2;j<=order-i;j++)
+            d[rmap(i,j,0)] = -Rm2*( (2*(i)-1)*x*d[rmap(i-1,j,0)] + (i-1)*(i-1)*d[rmap(i-2,j,0)] + 2*(j)*y*d[rmap(i,j-1,0)] + (j)*(j-1)*d[rmap(i,j-2,0)] );
+}
+
+
+void doradii(int radius, qd_real *dd) {
+    
+    qd_real rd[8192];
+
+    for(int m=0;m<2048;m++) dd[m] = 0;
+
+    for(int l1=-radius;l1<=radius;l1++)
+        for(int l2=-radius;l2<=radius;l2++)
+            for(int l3=-radius;l3<=radius;l3++) {
+                qd_real x2 = l1*l1 + l2*l2 + l3*l3;
+                if(x2>"0" && x2<= qd_real(radius*radius) ) {
+                    qd_real3 r;
+                    r.x = l1; r.y = l2; r.z = l3;
+                    QD_ab0(r,rd);
+                    for(int m=0;m<2048;m++) dd[m] += rd[m];
+                }
+            }
+
+}
+
+
+// make the far derivatives -- the analytic derivatives minus the 
+//   lattice sum of derivatives out to innerradius
+qd_real AnalyticDerivatives[ODIM][ODIM][ODIM];
+void MakeAnalyticDerivatives(int maxorder, int innerradius) {
+    assert(maxorder < ODIM);
+
+    GetAD(maxorder);
+
+    for(int a=0;a<ODIM;a++) 
+        for(int b=0;b<ODIM;b++) 
+            for(int c=0;c<ODIM;c++) 
+                AnalyticDerivatives[a][b][c] = 0;
+
+    for(int a=0;a<=maxorder;a+=2)
+        for(int b=0;b<=maxorder-a;b+=2) 
+                if( a+b>2) {
+                    AnalyticDerivatives[a][b][0] = AD[a][b];
+                }
+
+    qd_real rd2[8192];
+
+    doradii(innerradius, rd2);
+
+    for(int a=0;a<=maxorder;a+=2)
+        for(int b=0;b<=maxorder-a;b+=2)
+                if( a+b>2) {
+                    printf("% 2d % 2d\n", a, b);
+                    AnalyticDerivatives[a][b][0] -= rd2[ rmap(a,b,0) ];
+                }
+
+}
+
+
+int main(void) {
+   unsigned int old_cw;
+    fpu_fix_start(&old_cw);
+
+    cout.precision(qd_real::_ndigits);
+    int nd = qd_real::_ndigits + 8;
+
+    int order = 32;
+
+    makeEvenrmap();
+
+    for(int innerradius=8; innerradius<=16; innerradius*=2) {
+        printf("making innerradius = %d\n", innerradius);
+
+        MakeAnalyticDerivatives(order, innerradius);
+
+        char fname[1024];
+        sprintf(fname,"AD%02d_%03d.dat",order,innerradius);
+        cout << "fname = " << fname << endl;
+        ofstream fout(fname,ios::out);
+        fout.precision(qd_real::_ndigits);
+        
+        fout << order << "  " << innerradius << endl;
+        for(int a=0; a<=order; a+=2) {
+            for(int b=a; b<=order-a; b+=2) {
+                fout << setw(3) << a << "  " << setw(3) << b << "  ";
+                fout << setw(nd) << AnalyticDerivatives[a][b][0] << endl;
+            }
+        }
+        fout.close();
+    }
+
+    fpu_fix_end(&old_cw);
+}
