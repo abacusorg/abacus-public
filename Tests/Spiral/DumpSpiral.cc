2804046798a374d8c6960f8e83abe6b27a3e5e27
Doug Ferrer <dferrer@cfa.harvard.edu>
2013-03-08 12:48:04 -0500

    Added old spiral code. Still needs to be modified for our code

---
+++ Tests/Spiral/DumpSpiral.cc
@@ -0,0 +1,123 @@
+#define THISCODE std::string("Time-stamp: <DumpSpiral.cc on Saturday, 19 November, 2011 at 21:28:38 EST (bob)>")
+
+/*
+compile with:
+icc -o dumpspiral -I../ParseHeader -I../ParseHeader/util  -I../util -I../include -I.. DumpSpiral.cc ../util/stringutil.cc -L ../ParseHeader -lparseheader
+
+ */
+
+#include "header.h"
+#include "util.h"
+#include "threevector.hh"
+#include "ParseHeader.hh"
+#include "timestamp.cc"
+#include "stringutil.cc"
+
+typedef std::complex<double> Complex;
+typedef ThreeVector<double> double3;
+typedef ThreeVector<float> float3;
+typedef ThreeVector<int> int3;
+typedef long long int LLI;
+typedef unsigned long long int ULLI;
+
+using namespace std;
+
+#include "particles.h"
+#include "Grid.cc"
+#include "Spiral.cc"
+#include "spiralparameters.cc"
+#include "spiralspec.cc"
+#include "dimensionalization.cc"
+#include "Cosmology.cc"
+
+void Usage(char *progname) {
+    std::cout << "Usage: " << progname << ": -i infile -r rotfile\n";
+    abort();
+}
+
+void ParseCommandLine(int argc, char **argv, std::string &infile, std::string &rotfile) {
+    int c;
+    while ((c = getopt (argc, argv, "i:r:")) != -1)
+        switch (c)
+        {
+        case 'i':
+            infile.assign(optarg);
+            break;
+        case 'r':
+            rotfile.assign(optarg);
+            break;
+        case '?':
+            Usage(argv[0]);
+        default:
+            abort ();
+        }
+     
+    int error = 0;
+    for (int index = optind; index < argc; index++) {
+        std::cerr << "Non-option argument " << argv[index] << "\n";
+        error = 1;
+    }
+    if(error || infile.empty() || rotfile.empty()) Usage(argv[0]);
+}
+
+int main(int argc, char **argv) {
+
+    std::string datafile, rotfile;
+    ParseCommandLine(argc, argv, datafile, rotfile);
+
+    std::string infile(datafile);
+    
+    ParseHeader PH;
+
+    codeparam cp;
+    PH.register_vars(cp);
+    spiralparam sp;
+    PH.register_vars(sp);
+    HeaderStream instream(infile);
+    PH.ReadHeader(instream);
+
+    Spiral SP(cp.N1D, sp.kvec, sp.phase);
+
+    int np;
+    double anow;
+    fread(&anow, sizeof(double), 1, instream.fp);
+    fread(&np, sizeof(LLI), 1, instream.fp);
+    assert( np == cp.N1D*cp.N1D*cp.N1D );
+    particlestructure *ps = new particlestructure[np];
+    fread(ps, sizeof(particlestructure), np, instream.fp);
+    instream.Close();
+
+    MyCosmology cosmo;
+    cosmo.Omega_m = cp.Omega_M;
+    cosmo.Omega_K = cp.Omega_K;
+    cosmo.Omega_DE = cp.Omega_DE;
+    cosmo.H0 = cp.H0;
+    cosmo.w0 = cp.w0;
+    cosmo.wa = cp.wa;
+
+    Cosmology C(1e-3,cosmo);
+
+    //ConvertVpecToDimensionless(ps, np, cp, anow);
+    ConvertVzToDimensionless(ps, np, cp, anow, C.H(anow));
+
+    SP.Project(ps, np);
+
+    ConvertDimensionlessToVpec(ps, np, cp, anow);
+    //ConvertDimensionlessToVz(ps, np, cp, anow, C.H(anow));
+    cout << "H(" << anow << ") = " << C.H(anow) << "\n";
+
+    FILE *out = OpenForWrite(std::string(rotfile), true);
+    WriteHStream(out, "##\n", "# ");
+    WriteHStream(out, " Input parameters from " + instream.name + ":\n", "# ");
+    WriteHStream(out, "##\n", "# ");
+    WriteHStream(out, instream, "# ");
+    WriteHStream(out, "\n", "# " );
+    WriteHStream(out, "# by: " + THISCODE + "\n");
+    //    FinalizeHeader(out); // don't want the EOH token here...
+
+    for(int p=0; p<np; p++) {
+        fprintf(out, "% e % e\n", ps[p].position.x, ps[p].velocity.x);
+    }
+
+    fclose(out);
+}
---
+++ Tests/Spiral/plotrot.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+import time, sys
+from numpy import *
+from matplotlib.font_manager import FontProperties
+from pylab import *
+from math import acos, atan
+
+from matplotlib import rcParams
+
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['DroidSans']
+rcParams['text.usetex'] = True
+rcParams['font.size'] = 12.0
+rcParams['font.weight'] = 'bold'
+limits = array([[  0,   5, -225, 225],
+                [2.3, 2.7, -210, 210],
+                [2.47, 2.53,  -37,  37]])
+
+
+#get figure width from latex...
+fig_width = 5
+fig_height = 3
+fig = plt.figure(figsize=(fig_width,fig_height))
+fig.subplots_adjust(bottom=0.2,wspace=0.4)
+
+H0 = 50
+fac = 1/sqrt(3)   # norm of k-vector
+Lbox = 10/fac     # size of box for one wavelength in diagonal of 10 Mpc box
+mid = Lbox*fac/2
+print "mid = ", mid
+print mid-0.4, mid+0.4
+
+limits = array([[  0,   Lbox*fac, -225, 225],
+                [mid-0.05*Lbox*fac, mid+0.05*Lbox*fac, -225, 225],
+                [mid-0.007*Lbox*fac, mid+0.007*Lbox*fac,  -37,  37]])
+
+ax = []
+ax.append(subplot(131))
+filename = "spiral.rot"
+ld = loadtxt(filename)
+xd = ld[:,0]
+vd = ld[:,1]
+
+filename = "analyticspiral"
+l = loadtxt(filename)
+x = (l[:,0]+0.5)*Lbox/sqrt(3)
+v = l[:,1]*H0*Lbox/sqrt(3)
+
+ax[0].plot(x,v,linewidth=1)
+ax[0].plot(xd,vd,'r.',linewidth=1,markersize=2)
+ax[0].axis(limits[0]);
+tickpos = [int(i) for i in Lbox*fac*frange(0,1,.2)]
+ax[0].set_xticks(tickpos)
+minorx0 = MultipleLocator(2)
+minory0 = MultipleLocator(20)
+ax[0].xaxis.set_minor_locator(minorx0)
+ax[0].yaxis.set_minor_locator(minory0)
+ax[0].set_yticks((-200,-100,0,100,200))
+xlabel('x [Mpc]',size=12)
+ylabel('v [km s$^{-1}$]',size=12)
+
+ax.append(subplot(132))
+ax[1].plot(x,v,linewidth=1)
+ax[1].plot(xd,vd,'r.',linewidth=1,markersize=2)
+ax[1].axis(limits[1]);
+ax[1].set_xticks((4.6,5.0,5.4))
+minorx1 = MultipleLocator(.05)
+minory1 = MultipleLocator(20)
+ax[1].xaxis.set_minor_locator(minorx1)
+ax[1].yaxis.set_minor_locator(minory1)
+ax[1].set_yticks((-200,-100,0,100,200))
+xlabel('x [Mpc]',size=12)
+
+ax.append(subplot(133))
+ax[2].plot(x,v,linewidth=1)
+ax[2].plot(xd,vd,'r.',linewidth=1,markersize=2)
+ax[2].axis(limits[2]);
+ax[2].set_xticks((4.94,5.0,5.06))
+minorx2 = MultipleLocator(.01)
+minory2 = MultipleLocator(5)
+ax[2].xaxis.set_minor_locator(minorx2)
+ax[2].yaxis.set_minor_locator(minory2)
+ax[2].set_yticks((-30,0,30))
+xlabel('x [Mpc]',size=12)
+
+# set axis number font 
+font = FontProperties(family = 'Helvetica',size=12);
+for p in range(3):
+    ax[p].set_yticklabels(ax[p].get_yticks(), fontproperties = font)
+    ax[p].set_xticklabels(ax[p].get_xticks(), fontproperties = font)
+
+#savefig('figa.eps',format='eps',bbox_inches='tight')
+show()
