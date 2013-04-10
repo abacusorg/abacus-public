c5f44fb0b7646236ae37a17817117988b0b63467
Doug Ferrer <dferrer@cfa.harvard.edu>
2013-04-10 14:14:14 -0400

    actually fixed syntax

--- singlestep/singlestep.cpp
+++ singlestep/singlestep.cpp
@@ -285,8 +285,8 @@ void PlanOutput(bool MakeIC) {
 
 
 int main(int argc, char **argv) {
-	std::setvbuf(stdout,_IONBF,0,0);
-	std::setvbuf(err,_IONBF,0,0);
+	std::setvbuf(stdout,(char *)__IONBF,0,0);
+	std::setvbuf(stderr,(char *)_IONBF,0,0);
 
     WallClockDirect.Start();
     SingleStepSetup.Start();
