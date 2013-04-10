b4d2e9546337e0d561ece813d920724cebe3d610
Doug Ferrer <dferrer@cfa.harvard.edu>
2013-04-10 14:08:52 -0400

    setting stdout and stderr to be unbuffered

--- singlestep/singlestep.cpp
+++ singlestep/singlestep.cpp
@@ -285,6 +285,8 @@ void PlanOutput(bool MakeIC) {
 
 
 int main(int argc, char **argv) {
+	std::setvbuf(std::stdout,_IONBF,0,0);
+	std::setvbuf(std::err,_IONBF,0,0);
 
     WallClockDirect.Start();
     SingleStepSetup.Start();
