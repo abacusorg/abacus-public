9378bbc4ffd95921808b3d94be16d5336b4e72fd
Doug Ferrer <dferrer@cfa.harvard.edu>
2013-04-12 15:58:55 -0400

    reworked convolution driver to use new parameters struct

--- Convolution/convolutionwrapper.cpp
+++ Convolution/convolutionwrapper.cpp
@@ -20,42 +20,42 @@ STimer ConvolutionWallClock;
 
 #include <fenv.h>
 
-
 void dumpstats(OutofCoreConvolution *OCC, char *fn) {
 
     FILE *fp;
     fp = fopen(fn,"w");
     assert(fp!=NULL);
 
-    double accountedtime  = OCC->ConvolutionArithmetic.Elapsed();
-           accountedtime += OCC->ForwardZFFTMultipoles.Elapsed() + OCC->InverseZFFTTaylor.Elapsed();
-           accountedtime += OCC->ReadDerivatives.Elapsed() + OCC->ReadMultipoles.Elapsed() + OCC->WriteTaylor.Elapsed();
-           accountedtime += OCC->ArraySwizzle.Elapsed();
-    double discrepency = OCC->ConvolveWallClock.Elapsed() - accountedtime;
+
+    double accountedtime  = OCC->CS.ConvolutionArithmetic.Elapsed();
+           accountedtime += OCC->CS.ForwardZFFTMultipoles.Elapsed() + OCC->CS.InverseZFFTTaylor.Elapsed();
+           accountedtime += OCC->CS.ReadDerivatives.Elapsed() + OCC->CS.ReadMultipoles.Elapsed() + OCC->CS.WriteTaylor.Elapsed();
+           accountedtime += OCC->CS.ArraySwizzle.Elapsed();
+    double discrepency = OCC->CS.ConvolveWallClock.Elapsed() - accountedtime;
 
     int computecores = omp_get_num_procs()/2;
     fprintf(fp,"Convolution parameters:  RamAllocated = %dMB CacheSizeMB = %dMB nreal_cores=%d blocksize=%d zwidth=%d cpd=%d order=%d\n\n",
-        (int) (OCC->totalMemoryAllocated/(1<<20)), OCC->runtime_ConvolutionCacheSizeMB, computecores, OCC->blocksize, OCC->zwidth, OCC->runtime_cpd, OCC->runtime_order);
+        (int) (OCC->CS.totalMemoryAllocated/(1<<20)), OCC->CS.runtime_ConvolutionCacheSizeMB, computecores, OCC->CS.blocksize, OCC->CS.zwidth, OCC->CP.runtime_cpd, OCC->CP.runtime_order);
 
-    fprintf(fp,"\t ConvolutionWallClock:  %2.2e seconds \n", OCC->ConvolveWallClock.Elapsed() );
-    fprintf(fp,"\t \t \t %50s : %1.1e seconds\n", "Array Swizzling", OCC->ArraySwizzle.Elapsed() );
-    double e = OCC->ReadDerivativesBytes/OCC->ReadDerivatives.Elapsed()/(1.0e+6);
-    fprintf(fp,"\t \t \t %50s : %1.1e seconds --> rate was %4d MB/s\n", "ReadDiskDerivatives", OCC->ReadDerivatives.Elapsed(), (int) (e) );
-    e = OCC->ReadMultipolesBytes/OCC->ReadMultipoles.Elapsed()/(1.0e+6);
-    fprintf(fp,"\t \t \t %50s : %1.1e seconds --> rate was %4d MB/s\n", "ReadDiskMultipoles", OCC->ReadMultipoles.Elapsed(), (int) (e) );
-    e = OCC->WriteTaylorBytes/OCC->WriteTaylor.Elapsed()/(1.0e+6);
-    fprintf(fp,"\t \t \t %50s : %1.1e seconds --> rate was %4d MB/s\n", "WriteDiskTaylor", OCC->WriteTaylor.Elapsed(), (int) (e) );
-    double Gops = ((double) OCC->ops)/(1.0e+9);
-    fprintf(fp,"\t \t \t %50s : %1.1e seconds for %5.3f billion double precision operations\n", "Convolution Arithmetic", OCC->ConvolutionArithmetic.Elapsed(), Gops );
-    fprintf(fp,"\t \t \t %50s : %1.1e seconds\n", "Forward FFT Z Multipoles", OCC->ForwardZFFTMultipoles.Elapsed() );
-    fprintf(fp,"\t \t \t %50s : %1.1e seconds\n", "Inverse FFT Z Taylor",         OCC->InverseZFFTTaylor.Elapsed() );
-    fprintf(fp,"\t \t %50s : %1.1e seconds which is %d%% \n", "Unaccounted remaining wallclock time", discrepency, (int) (discrepency/OCC->ConvolveWallClock.Elapsed()*100) );
+    fprintf(fp,"\t ConvolutionWallClock:  %2.2e seconds \n", OCC->CS.ConvolveWallClock.Elapsed() );
+    fprintf(fp,"\t \t \t %50s : %1.1e seconds\n", "Array Swizzling", OCC->CS.ArraySwizzle.Elapsed() );
+    double e = OCC->CS.ReadDerivativesBytes/OCC->CS.ReadDerivatives.Elapsed()/(1.0e+6);
+    fprintf(fp,"\t \t \t %50s : %1.1e seconds --> rate was %4d MB/s\n", "ReadDiskDerivatives", OCC->CS.ReadDerivatives.Elapsed(), (int) (e) );
+    e = OCC->CS.ReadMultipolesBytes/OCC->CS.ReadMultipoles.Elapsed()/(1.0e+6);
+    fprintf(fp,"\t \t \t %50s : %1.1e seconds --> rate was %4d MB/s\n", "ReadDiskMultipoles", OCC->CS.ReadMultipoles.Elapsed(), (int) (e) );
+    e = OCC->CS.WriteTaylorBytes/OCC->CS.WriteTaylor.Elapsed()/(1.0e+6);
+    fprintf(fp,"\t \t \t %50s : %1.1e seconds --> rate was %4d MB/s\n", "WriteDiskTaylor", OCC->CS.WriteTaylor.Elapsed(), (int) (e) );
+    double Gops = ((double) OCC->CS.ops)/(1.0e+9);
+    fprintf(fp,"\t \t \t %50s : %1.1e seconds for %5.3f billion double precision operations\n", "Convolution Arithmetic", OCC->CS.ConvolutionArithmetic.Elapsed(), Gops );
+    fprintf(fp,"\t \t \t %50s : %1.1e seconds\n", "Forward FFT Z Multipoles", OCC->CS.ForwardZFFTMultipoles.Elapsed() );
+    fprintf(fp,"\t \t \t %50s : %1.1e seconds\n", "Inverse FFT Z Taylor",         OCC->CS.InverseZFFTTaylor.Elapsed() );
+    fprintf(fp,"\t \t %50s : %1.1e seconds which is %d%% \n", "Unaccounted remaining wallclock time", discrepency, (int) (discrepency/OCC->CS.ConvolveWallClock.Elapsed()*100) );
 
-    double cae = OCC->ConvolutionArithmetic.Elapsed();
-    int farithp   = cae/OCC->ConvolveWallClock.Elapsed()*100;
-    int ffftp     = (OCC->ForwardZFFTMultipoles.Elapsed() + OCC->InverseZFFTTaylor.Elapsed())/OCC->ConvolveWallClock.Elapsed()*100;
-    int fiop      = (OCC->ReadDerivatives.Elapsed() + OCC->ReadMultipoles.Elapsed() + OCC->WriteTaylor.Elapsed() )/OCC->ConvolveWallClock.Elapsed()*100;
-    int swzp      = OCC->ArraySwizzle.Elapsed()/OCC->ConvolveWallClock.Elapsed()*100;
+    double cae = OCC->CS.ConvolutionArithmetic.Elapsed();
+    int farithp   = cae/OCC->CS.ConvolveWallClock.Elapsed()*100;
+    int ffftp     = (OCC->CS.ForwardZFFTMultipoles.Elapsed() + OCC->CS.InverseZFFTTaylor.Elapsed())/OCC->CS.ConvolveWallClock.Elapsed()*100;
+    int fiop      = (OCC->CS.ReadDerivatives.Elapsed() + OCC->CS.ReadMultipoles.Elapsed() + OCC->CS.WriteTaylor.Elapsed() )/OCC->CS.ConvolveWallClock.Elapsed()*100;
+    int swzp      = OCC->CS.ArraySwizzle.Elapsed()/OCC->CS.ConvolveWallClock.Elapsed()*100;
 
     fprintf(fp,"\t Summary: Fourier Transforms = %d%%     Convolution Arithmetic = %d%%    DiskIO = %d%%   Array Swizzle = %d%% \n", ffftp, farithp, fiop, swzp );
     fprintf(fp,"\t          Arithmetic rate = %d DGOPS --> rate per core = %1.1f DGOPS\n", (int) (Gops/cae), Gops/cae/computecores );
@@ -84,15 +84,28 @@ int main(int argc, char ** argv){
 	    sprintf(logfn,"%s/lastconvolution.log", P.LogFileDirectory);
 	    stdlog.open(logfn);
 	    OutofCoreConvolution OCC;
-	    STDLOG(1,"Read parameter file\n")
+	    STDLOG(1,"Read parameter file\n");
 	    Setup.Stop();
+
+	    ConvolutionParameters p;
+	    p.runtime_ConvolutionCacheSizeMB = P.ConvolutionCacheSizeMB;
+	    p.runtime_DerivativeExpansionRadius = P.DerivativeExpansionRadius;
+	    strcpy(P.DerivativesDirectory,p.runtime_DerivativesDirectory);
+	    p.runtime_DiskBufferSizeKB = 4;
+	    p.runtime_IsRamDisk = P.RamDisk;
+	    p.runtime_MaxConvolutionRAMMB = P.MAXConvolutionRAMMB;
+	    strcpy(P.ReadStateDirectory,p.runtime_MultipoleDirectory);
+	    p.runtime_MultipolePrefix = "Multipoles";
+	    p.runtime_NearFieldRadius = P.NearFieldRadius;
+	    strcpy(P.WriteStateDirectory,p.runtime_TaylorDirectory);
+	    p.runtime_cpd = P.cpd;
+	    p.runtime_order = P.order;
+
+
 	    ConvolutionWallClock.Start();
 	    STDLOG(1,"Starting Convolution\n");
-	    OCC.Convolve( P.cpd, P.order, P.NearFieldRadius, P.DerivativeExpansionRadius,
-	                      P.RamDisk, 4,  P.ConvolutionCacheSizeMB, P.MAXConvolutionRAMMB,
-	                      P.DerivativesDirectory, P.ReadStateDirectory, P.WriteStateDirectory,
-	                      "Multipoles", "Taylor" );
-	    STDLOG(1,"Convolution Complete\n")
+	    OCC.Convolve(p);
+	    STDLOG(1,"Convolution Complete\n");
 	    ConvolutionWallClock.Stop();
 	    TotalWallClock.Stop();
 	    char timingfn[1050];
