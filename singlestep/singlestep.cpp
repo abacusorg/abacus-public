06b4a0cbbd5b5f29d05ef43945a9eb4a493f85f8
Doug Ferrer <doug@charlie.private>
2013-08-03 22:44:22 -0400

    trying to fix ewald

--- singlestep/singlestep.cpp
+++ singlestep/singlestep.cpp
@@ -298,7 +298,7 @@ int main(int argc, char **argv) {
 	std::setvbuf(stdout,(char *)_IONBF,0,0);
 	std::setvbuf(stderr,(char *)_IONBF,0,0);
 
-	ompc_set_num_threads(6);
+	omp_set_num_threads(6);
 
     WallClockDirect.Start();
     SingleStepSetup.Start();
