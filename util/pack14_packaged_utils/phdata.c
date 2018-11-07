9fcc24a0a58233c1751c61b5d4afd4e0dae17342
Lehman Garrison <lgarrison@cfa.harvard.edu>
2018-11-07 11:42:38 -0500

    Add pre-packaged pack14 tarball suitable for distribution

---
+++ util/pack14_packaged_utils/phdata.c
@@ -0,0 +1,20 @@
+/* phdata -- Skip the ParseHeader header of a file and print the rest to stdout.
+If no filename is given, read from stdin.  
+If many file names are given, perform on all of them.
+*/
+
+#define PROGNAME "phdata"
+#include "util_header.h"
+#include "util_main.c"
+
+
+void print_data(FILE *fp) {
+    int c, clast;
+    if ((clast=getc(fp))==EOF) return;
+    while ((c=getc(fp))!=EOF) {
+        if (clast==''&&c=='\n') break;
+        clast = c;
+    }
+    // Now we're found the end of the header; continue.
+    while ((c=getc(fp))!=EOF) putchar(c);   
+}
---
+++ util/pack14_packaged_utils/phheader.c
@@ -0,0 +1,23 @@
+/* phheader -- Print the ParseHeader header of a file to stdout.
+This routine doesn't do any parsing -- just echos the text, 
+stripping out the final demarker (^B\n).  We add a \n to the end of the file.
+If no filename is given, read from stdin.  
+If many file names are given, perform on all of them.
+*/
+
+#define PROGNAME "phheader"
+
+#include "util_header.h"
+#include "util_main.c"
+
+
+void print_data(FILE *fp) {
+    int c, clast;
+    if ((clast=getc(fp))==EOF) return;
+    while ((c=getc(fp))!=EOF) {
+        if (clast==''&&c=='\n') { putchar('\n'); return; }
+        putchar(clast); clast = c;
+    }
+    putchar(clast);   // If the demarker hasn't been found, we still have one more char
+    putchar('\n');    // Add a \n as a formatting guard.
+}
