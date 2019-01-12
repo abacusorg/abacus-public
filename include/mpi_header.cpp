20e2b57654aec1eac654920c98be438ae26f93a7
Daniel Eisenstein <deisenstein@cfa.harvard.edu>
2019-01-12 10:07:29 -0500

    Adding MPI_Allreduce as a commented option
    But my tests are that this doesn't affect the current crashing

--- include/mpi_header.cpp
+++ include/mpi_header.cpp
@@ -4,6 +4,7 @@
     #include "mpi.h"
     // This does an in-place reduction to rank 0
     #define MPI_REDUCE_TO_ZERO(vec,len,type,op) MPI_Reduce(MPI_rank!=0?(vec):MPI_IN_PLACE, vec, len, type, op, 0, MPI_COMM_WORLD)
+    //#define MPI_REDUCE_TO_ZERO(vec,len,type,op) MPI_Allreduce(MPI_IN_PLACE, vec, len, type, op, MPI_COMM_WORLD)
 #else
     // Just put in some stubs to help compilation
     typedef void * MPI_Request;
