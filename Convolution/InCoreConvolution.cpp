00eef055b3ab80cf1d07fa910a27ba04e5891b06
Daniel Eisenstein <deisenstein@cfa.harvard.edu>
2019-07-18 12:48:13 -0400

    Small optimizations

--- Convolution/InCoreConvolution.cpp
+++ Convolution/InCoreConvolution.cpp
@@ -18,7 +18,7 @@ public:
         We've been given a blocksize, and at present it must divide CPD^2
         evenly, which means that it must be odd.  
         However, we'd like to pad each multipole out to a multiple of 4,
-        so that even doubles are 32-byte aligned.
+        so that doubles are 32-byte aligned and can use AVX.
         */
         padblocksize = (blocksize/4+1)*4;
 
@@ -64,48 +64,34 @@ public:
 unsigned long long int InCoreConvolution::ConvolutionArithmeticCount(void) {
     int a,b,c,i,j,k;
 
+    // Not counting math to unwrap the derivatives during loading.
+    // Not counting math to set up the loops and pointers.
     unsigned long long int l = 0;
+    unsigned long long int cpd3 = ((unsigned long long int ) cpd)*cpd*(1+cpd)/2;
 
-    /// This appears to be a bug
-    unsigned long long int cpd3 = ((unsigned long long int ) cpd)*cpd*cpd;
-    /// unsigned long long int cpd3 = ((unsigned long long int ) cpd)*cpd*(1+cpd)/2;
-
-    FORALL_REDUCED_MULTIPOLES_BOUND(a,b,c,order) {
-
-            FORALL_COMPLETE_MULTIPOLES_BOUND(i,j,k,order-a-b-c) {
-                if( ((a+i)+(b+j)+(c+k))%2 ==0 ) {
-                    l += cpd3 * 4;
-                }
-            }
-
-            FORALL_COMPLETE_MULTIPOLES_BOUND(i,j,k,order-a-b-c) {
-                if( ((a+i)+(b+j)+(c+k))%2 == 1 ) {
-                    l += cpd3* 4;
-                }
-            }
-
-            l += cpd3*4; // complex multiply
+    TRACEFREERECURSION(a,b,c,order) { 
+        l+=4;   // Multipole tracefree recursion
+        l+=2;   // Derivative tracefree recursion
+    }
+    FORALL_COMPLETE_MULTIPOLES_BOUND(a,b,c,order)  {
+        l+=2;   // Multipole rescaling
     }
 
-    return l;
+    FORALL_REDUCED_MULTIPOLES_BOUND(a,b,c,order) {
+        FORALL_COMPLETE_MULTIPOLES_BOUND(i,j,k,order-a-b-c) {
+            l += 4;  // MVM work
+        }
+        l += 2; // multiply by I
+    }
 
-}
-
-inline void MVMpair(Complex *t, Complex *m, double *d, int n) { 
-    // We require here that d[] is already broadcast into a list of pairs
-    // of doubles, so that we can do the FMA math as a pure vector of doubles.
-    double *td = (double *)t;
-    double *md = (double *)m;
-    n*=2;
-    #pragma simd assert
-    for(int i=0;i<n;i++) td[i] += md[i] * d[i]; 
+    return l*cpd3;
 }
 
 inline void MVM(Complex *t, Complex *m, double *d, int n) { 
 
     #ifdef HAVE_AVX
-    // An AVX equivalent -- this does two at once, so we have to pad!
-    // Will compute off the end
+    // An AVX equivalent -- this does two at once, so we have to pad to 2!
+    // Will compute off the end.  
     for (int i=0; i<n; i+=2) {
         __m256d _xx = _mm256_broadcast_pd((__m128d *)(d+i));
             // So now we have _xx = (c,d,c,d).
@@ -120,12 +106,12 @@ inline void MVM(Complex *t, Complex *m, double *d, int n) {
         _mm256_store_pd((double *)(t+i), zz);
     }
     #else
-    #pragma omp simd aligned(t,m:16) aligned(d:8)
-    for(int i=0;i<n;i++) t[i] += m[i] * d[i]; 
+        #pragma omp simd aligned(t,m:16) aligned(d:8)
+        for(int i=0;i<n;i++) t[i] += m[i] * d[i]; 
     #endif
 
     /*
-    // An SSE equivalent
+    // An SSE equivalent, as an example for VSX
     for (int i=0; i<n; i++) {
         __m128d xx = _mm_load1_pd((double *)(d+i));
         __m128d yy = _mm_load_pd((double *)(m+i));
@@ -136,6 +122,28 @@ inline void MVM(Complex *t, Complex *m, double *d, int n) {
     }
     */
 
+}
+
+inline void Multiply_by_I(Complex *t, int blocksize) {
+    // Multiplying by i really means swapping to (-im, re)
+    int xyz;
+    #ifndef HAVE_AVX
+        double *tcache_dbl = (double *)t;
+        for (xyz=0; xyz<2*blocksize; xyz+=2) {
+            double tmp = tcache_dbl[xyz];
+            tcache_dbl[xyz] = -tcache_dbl[xyz+1];
+            tcache_dbl[xyz+1] = tmp;
+        }
+    #else
+        // Consider using SSE intrinsics to do this flip
+        __m128d neg = _mm_set_pd(0.0d, -0.0d);
+        for (xyz=0; xyz<blocksize; xyz++) {
+            __m128d tswap = _mm_load_pd((double *)(t+xyz));
+            tswap = _mm_shuffle_pd(tswap,tswap,0x1);
+            tswap = _mm_xor_pd(tswap, neg);
+            _mm_store_pd((double *)(t+xyz),tswap);
+        }
+    #endif
 }
 
 void InCoreConvolution::InCoreConvolve(Complex *FFTM, DFLOAT *CompressedD) {
@@ -173,8 +181,9 @@ void InCoreConvolution::InCoreConvolve(Complex *FFTM, DFLOAT *CompressedD) {
 
         // Apply the scaling to the complete multipoles
         FORALL_COMPLETE_MULTIPOLES_BOUND(a,b,c,order)  {
-            FM = &(mcache[cmap(a,b,c) * padblocksize]);
-            double mf = mfactor[ cmap(a,b,c) ];
+            int cmap_abc = cmap(a,b,c);
+            FM = &(mcache[cmap_abc * padblocksize]);
+            double mf = mfactor[ cmap_abc ];
             FOR(xyz,0,blocksize-1) FM[xyz] *= mf;
         }
 
@@ -233,18 +242,6 @@ void InCoreConvolution::InCoreConvolve(Complex *FFTM, DFLOAT *CompressedD) {
                 FDabc[xyz] = - FDap2bcm2[xyz] - FDabp2cm2[xyz];
         }
 
-/*
-        // Now we want to broadcast each double into two, so that
-        // the MVM math is trivial double-FMA.  Until now, dcache
-        // has only used the first half of its buffer, length
-        // completemultipolelength*blocksize.  Now we want to double
-        // that.
-        FDd = dcache+completemultipolelength*blocksize*2-2;
-        for (xyz=completemultipolelength*blocksize-1; xyz>=0; xyz--, FDd-=2) {
-            FDd[1] = FDd[0] = dcache[xyz];
-        }
-*/
-
         // Now compute the Taylors from M & D
         // We do this in two parts, since the D's are stored as real,
         // but are either pure real or pure imaginary, according
@@ -256,22 +253,21 @@ void InCoreConvolution::InCoreConvolve(Complex *FFTM, DFLOAT *CompressedD) {
                 if( (((a+i)+(b+j)+(c+k))&0x1) == 1 ) {
                     FD  = &(dcache[cmap(a+i,b+j,c+k) * padblocksize]);
                     FM  = &(mcache[cmap(i  ,j  ,k  ) * padblocksize]);
+                    // We know the rest of the k's are simple
+                    for (; k<=order-a-b-c-i-j; k+=2, FD+=padblocksize*2, FM+=padblocksize*2)
                     MVM(tcache, FM, FD, blocksize);
                 }
             }
             // Now multiply by i, before we accumulate the rest
-            double *tcache_dbl = (double *)tcache;
-            for (xyz=0; xyz<2*blocksize; xyz+=2) {
-                double tmp = tcache_dbl[xyz];
-                tcache_dbl[xyz] = -tcache_dbl[xyz+1];
-                tcache_dbl[xyz+1] = tmp;
-            }
+            Multiply_by_I(tcache, blocksize);
 
             FORALL_COMPLETE_MULTIPOLES_BOUND(i,j,k,order-a-b-c) {
                 // Even parity only 
                 if( (((a+i)+(b+j)+(c+k))&0x1) ==0 ) {
                     FD  = &(dcache[cmap(a+i,b+j,c+k) * padblocksize]);
                     FM  = &(mcache[cmap(i  ,j  ,k  ) * padblocksize]);
+                    // We know the rest of the k's are simple
+                    for (; k<=order-a-b-c-i-j; k+=2, FD+=padblocksize*2, FM+=padblocksize*2)
                     MVM(tcache,FM, FD, blocksize); 
                 }
             }
