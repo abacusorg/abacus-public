c2f32a46b7f1a5122adcc57d8fff1ac303aa3a27
Daniel Eisenstein <deisenstein@cfa.harvard.edu>
2019-08-02 16:26:07 -0400

    Adding PTIMER_DUMMY option
    This will turn off PTimer and cause Elapsed() to return 1e-12 second.

--- include/PTimer.cc
+++ include/PTimer.cc
@@ -19,6 +19,18 @@ PTimer::~PTimer() {
     free(timer);
 }
 
+void PTimer::Clear(void) {
+    for(int g=0; g<nprocs; g++)  assert(!timer[g].on);  
+    for(int g=0; g<nprocs; g++) {
+        timer[g].tot.tv_sec = 0; timer[g].tot.tv_nsec = 0;
+    }
+}
+
+#ifndef PTIMER_DUMMY
+// PTimer is expensive enough (estimate is about 35 ns per call on 'ted')
+// to notice, because it's used only in parallel loops where we're often
+// working on relatively small chunks.  So we provide a way to no-op these.
+
 inline void PTimer::Start(void){
     Start(omp_get_thread_num());
 }
@@ -53,16 +65,18 @@ inline void PTimer::Stop(int thread_num) {
     timer[g].on = 0;
 }
 
-void PTimer::Clear(void) {
-    for(int g=0; g<nprocs; g++)  assert(!timer[g].on);  
-    for(int g=0; g<nprocs; g++) {
-        timer[g].tot.tv_sec = 0; timer[g].tot.tv_nsec = 0;
-    }
-}
-
 double PTimer::Elapsed(void) {
     for(int g=0; g<nprocs; g++)  assert(!timer[g].on);  
     double sum = 0;
     for(int g=0; g<nprocs; g++) sum += timer[g].tot.tv_sec + 1e-9*timer[g].tot.tv_nsec;
     return sum;
 }
+
+#else
+inline void PTimer::Start(void){ return; }
+inline void PTimer::Start(int thread_num) { return; }
+inline void PTimer::Stop(void){ return; }
+inline void PTimer::Stop(int thread_num) { return; }
+inline double PTimer::Elapsed(void){ return 1e-12; }
+    // We return a small number so that we don't divide by zero
+#endif
