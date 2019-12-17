e86a633cb0c7b718182037316c5ec8929f774489
Lehman Garrison <lgarrison@flatironinstitute.org>
2019-12-17 14:02:31 -0500

    Fix two globalgroup bugs

--- singlestep/FOF/globalgroup.cpp
+++ singlestep/FOF/globalgroup.cpp
@@ -946,7 +946,7 @@ void GlobalGroupSlab::FindSubGroups() {
                     
                             FOFparticle *L2start = FOFlevel2[g].p + FOFlevel2[g].groups[0].start;
                             for (int p=0; p<FOFlevel2[g].groups[0].n; p++) {
-                                if (groupaux[start[L2start[p].index()].index()].is_taggable()); //1 or 2 = taggable. 0 = not taggable. 
+                                if (groupaux[start[L2start[p].index()].index()].is_taggable()) //1 or 2 = taggable. 0 = not taggable. 
                                     groupaux[start[L2start[p].index()].index()].set_tagged();                
                             }
                         }
@@ -992,7 +992,7 @@ void GlobalGroupSlab::FindSubGroups() {
                         h.SO_radius = sqrt(FOFlevel1[g].groups[a].halo_thresh2) / FOF_RESCALE; 
 
                         //now repeat for the largest L2 halo.
-                        posstruct L2max_central_particle = WrapPosition(L1pos[g][FOFlevel2[g][FOFlevel2[g].groups[0].start].index()] + offset);
+                        posstruct L2max_central_particle = WrapPosition(L1pos[g][FOFlevel2[g].p[FOFlevel2[g].groups[0].start].index()] + offset);
 
                         h.SO_L2max_central_particle[0] = L2max_central_particle.x;
                         h.SO_L2max_central_particle[1] = L2max_central_particle.y;
