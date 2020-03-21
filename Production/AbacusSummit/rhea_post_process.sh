9b4e47b10f028e1de575fe58d42588316c3cc305
Lehman Garrison <lgarrison@cfa.harvard.edu>
2020-03-21 10:43:16 -0400

    Last trz commit, before porting to blosc

--- Production/AbacusSummit/rhea_post_process.sh
+++ Production/AbacusSummit/rhea_post_process.sh
@@ -16,16 +16,13 @@ DISBATCH_TASKFILE=$(pwd)/tmp/${SIM_NAME}.group.disbatch
 
 CHUNK=50
 NWORKER=5
-# TODO: try lz4
 GROUPSCRIPT="$ABACUS/Abacus/convert_raw_groups_to_asdf.py --delete --chunk=$CHUNK --nworkers=$NWORKER"
 GROUPDIR=$ABACUSSUMMIT_PERSIST/$SIM_SET/$SIM_NAME/group
 
 echo "#DISBATCH PREFIX cd $GROUPDIR; $GROUPSCRIPT " > $DISBATCH_TASKFILE  # write
 echo "#DISBATCH SUFFIX > $(pwd)/\${DISBATCH_NAMETASKS}_\${DISBATCH_JOBID}_\${DISBATCH_TASKID}.log 2>&1" >> $DISBATCH_TASKFILE  # append
 
-cd $GROUPDIR
-echo Step*/ | tr " " "\n" >> $DISBATCH_TASKFILE
-cd - > /dev/null
+(cd $GROUPDIR; echo Step*/) | tr " " "\n" >> $DISBATCH_TASKFILE
 
 mkdir -p ./logs/$SIM_NAME/disbatchGroups
 
@@ -33,10 +30,10 @@ mkdir -p ./logs/$SIM_NAME/disbatchGroups
 NNODES=16
 WALLTIME=3:00:00
 # 1 task per node. Each task uses 5 workers, each running 3 processes.
-#JOBID1=$(sbatch -t $WALLTIME -N $NNODES --ntasks-per-node=1 \
-#        --mem=0 -p batch -A AST145 --parsable --job-name=${SIM_NAME}_PostProcessGroups -o logs/$SIM_NAME/%x.out \
-#        --wrap "./disBatch/disBatch.py -e -p ./logs/$SIM_NAME/disbatchGroups/$SIM_NAME $DISBATCH_TASKFILE && echo Groups completed successfully.")
-#EPILOG_DEPEND+=":$JOBID1"
+JOBID1=$(sbatch -t $WALLTIME -N $NNODES --ntasks-per-node=1 \
+        --mem=0 -p batch -A AST145 --parsable --job-name=${SIM_NAME}_PostProcessGroups -o logs/$SIM_NAME/%x.out \
+        --wrap "./disBatch/disBatch.py -e -p ./logs/$SIM_NAME/disbatchGroups/$SIM_NAME $DISBATCH_TASKFILE && echo Groups completed successfully.")
+EPILOG_DEPEND+=":$JOBID1"
 
 ##### queue up lightcone processing #####
 
@@ -51,9 +48,7 @@ echo "#DISBATCH PREFIX cd $LCDIR; $LCSCRIPT " > $DISBATCH_TASKFILE  # write
 echo "#DISBATCH SUFFIX > $(pwd)/\${DISBATCH_NAMETASKS}_\${DISBATCH_JOBID}_\${DISBATCH_TASKID}.log 2>&1 " >> $DISBATCH_TASKFILE  # append
 
 # Args could get dangerously long, use relative paths
-cd $LCDIR
-find Step*/ -name 'LightCone*' | cut -d. -f1 | sort -u >> $DISBATCH_TASKFILE  # Step0500/LightCone0_heal, etc
-cd - > /dev/null
+#(cd $LCDIR; find Step*/ -name 'LightCone*' | cut -d. -f1 | sort -u) >> $DISBATCH_TASKFILE  # Step0500/LightCone0_heal, etc
 
 # Now generate the checksum concatenation tasks
 
@@ -80,24 +75,63 @@ EOM
 NNODES=16
 WALLTIME=1:00:00
 
-mkdir -p ./logs/$SIM_NAME/disbatchLC
+#mkdir -p ./logs/$SIM_NAME/disbatchLC
 
 # 6 cores per task: only three processes per task, but cores have two hyperthreads.  So 5 tasks per node.
-JOBID2=$(sbatch -t $WALLTIME -N $NNODES -c 6 \
-        -o logs/$SIM_NAME/%x.out --mem=0 -A AST145 -p batch --parsable --job-name=${SIM_NAME}_PostProcessLC \
-        --wrap "./disBatch/disBatch.py -e -p ./logs/$SIM_NAME/disbatchLC/$SIM_NAME $DISBATCH_TASKFILE && echo Lightcones completed successfully.")
-EPILOG_DEPEND+=":$JOBID2"
+#JOBID2=$(sbatch -t $WALLTIME -N $NNODES -c 6 \
+#        -o logs/$SIM_NAME/%x.out --mem=0 -A AST145 -p batch --parsable --job-name=${SIM_NAME}_PostProcessLC \
+#        --wrap "./disBatch/disBatch.py -e -p ./logs/$SIM_NAME/disbatchLC/$SIM_NAME $DISBATCH_TASKFILE && echo Lightcones completed successfully.")
+#EPILOG_DEPEND+=":$JOBID2"
 
 ##### Queue up the time slice processing #####
 
-if [[ have timeslices ]]; then
+SIMDIR=$ABACUSSUMMIT_PERSIST/$SIM_SET/$SIM_NAME
+SLICEDIRS=$(cd $SIMDIR; find -maxdepth 1 -name 'slice*.*')
+
+if [[ -n "$SLICEDIRS" ]]; then
 
     # Write a disBatch file with one line per chunk per file type per time slice
     # Chunks should be specified with a literal pattern like slice1.000/*slab001?.L0_pack9.dat
     DISBATCH_TASKFILE=$(pwd)/tmp/${SIM_NAME}.timeslice.disbatch
 
+    TSSCRIPT=$(pwd)/rhea_post_process_timeslice.sh
+    echo "#DISBATCH PREFIX cd ${SIMDIR}; ${TSSCRIPT} \"" > $DISBATCH_TASKFILE  # write
+    echo "#DISBATCH SUFFIX \"" >> $DISBATCH_TASKFILE  # append
+
+    for SLICE in $SLICEDIRS; do
+        # Write one line for every chunk of 10 for every file type
+        # slice1.000/*slab001?.L0_pack9.dat
+
+        i=0
+        while : ; do
+            FNPREFIX="${SLICE}/$(printf '*slab%03d?*.dat' $i)"
+            MATCHES=$(cd $SIMDIR; find -wholename "$FNPREFIX" -print -quit)
+            if [[ -z "$MATCHES" ]]; then
+                break
+            fi
+            for FTYPE in L0_pack9 L0_pack9_pids field_pack9 field_pack9_pids; do
+                echo "${SLICE}"/$(printf "*slab%03d?.${FTYPE}.dat" $i) >> $DISBATCH_TASKFILE
+            done
+            i=$((i + 1))
+        done
+    done
+
+    # Now generate the checksum concatenation tasks
+    TSEPI_SCRIPT=$(pwd)/rhea_post_process_timeslice_epilogue.sh
+    cat >> $DISBATCH_TASKFILE << EOM
+#DISBATCH BARRIER CHECK
+#DISBATCH PREFIX 
+#DISBATCH SUFFIX 
+$TSEPI_SCRIPT $SIMDIR/slices/z*
+EOM
+
+    cat >> $DISBATCH_TASKFILE << EOM
+#DISBATCH BARRIER CHECK
+rm -rf $SIMDIR/slice*.*
+EOM
+
     # We can fill up to ??? nodes
-    NNODES=16
+    NNODES=3
     WALLTIME=1:00:00
 
     mkdir -p ./logs/$SIM_NAME/disbatchTS
