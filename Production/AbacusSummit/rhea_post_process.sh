e43d424967fbff08ccb717b80871f8f65a43149e
Lehman Garrison <lgarrison@cfa.harvard.edu>
2020-03-25 18:22:33 -0400

    Convert assembly line to ASDF+blosc

--- Production/AbacusSummit/rhea_post_process.sh
+++ Production/AbacusSummit/rhea_post_process.sh
@@ -15,8 +15,8 @@ mkdir -p tmp
 DISBATCH_TASKFILE=$(pwd)/tmp/${SIM_NAME}.group.disbatch
 
 CHUNK=50
-NWORKER=5
-GROUPSCRIPT="$ABACUS/Abacus/convert_raw_groups_to_asdf.py --delete --chunk=$CHUNK --nworkers=$NWORKER"
+NWORKER=8
+GROUPSCRIPT="$ABACUS/Abacus/convert_raw_groups_to_asdf.py --chunk=$CHUNK --nworkers=$NWORKER"
 GROUPDIR=$ABACUSSUMMIT_PERSIST/$SIM_SET/$SIM_NAME/group
 
 echo "#DISBATCH PREFIX cd $GROUPDIR; $GROUPSCRIPT " > $DISBATCH_TASKFILE  # write
@@ -27,9 +27,9 @@ echo "#DISBATCH SUFFIX > $(pwd)/\${DISBATCH_NAMETASKS}_\${DISBATCH_JOBID}_\${DIS
 mkdir -p ./logs/$SIM_NAME/disbatchGroups
 
 # We can fill up to ~36 nodes
-NNODES=16
+NNODES=3
 WALLTIME=3:00:00
-# 1 task per node. Each task uses 5 workers, each running 3 processes.
+# 1 task per node. Each task uses 8 workers, each running 2 blosc threads
 JOBID1=$(sbatch -t $WALLTIME -N $NNODES --ntasks-per-node=1 \
         --mem=0 -p batch -A AST145 --parsable --job-name=${SIM_NAME}_PostProcessGroups -o logs/$SIM_NAME/%x.out \
         --wrap "./disBatch/disBatch.py -e -p ./logs/$SIM_NAME/disbatchGroups/$SIM_NAME $DISBATCH_TASKFILE && echo Groups completed successfully.")
@@ -37,23 +37,26 @@ EPILOG_DEPEND+=":$JOBID1"
 
 ##### queue up lightcone processing #####
 
-# Write a disBatch file with one line per light cone per file type per step
-DISBATCH_TASKFILE=$(pwd)/tmp/${SIM_NAME}.LC.disbatch
-
 LCDIR=$ABACUSSUMMIT_PERSIST/$SIM_SET/$SIM_NAME/lightcone/
-NEWLCDIR=$ABACUSSUMMIT_PERSIST/$SIM_SET/$SIM_NAME/lightcones.trz/
 
-LCSCRIPT=$(pwd)/rhea_post_process_lightcones.sh
-echo "#DISBATCH PREFIX cd $LCDIR; $LCSCRIPT " > $DISBATCH_TASKFILE  # write
-echo "#DISBATCH SUFFIX > $(pwd)/\${DISBATCH_NAMETASKS}_\${DISBATCH_JOBID}_\${DISBATCH_TASKID}.log 2>&1 " >> $DISBATCH_TASKFILE  # append
+if [ -d $LCDIR ]; then
 
-# Args could get dangerously long, use relative paths
-#(cd $LCDIR; find Step*/ -name 'LightCone*' | cut -d. -f1 | sort -u) >> $DISBATCH_TASKFILE  # Step0500/LightCone0_heal, etc
+    # Write a disBatch file with one line per light cone per file type per step
+    DISBATCH_TASKFILE=$(pwd)/tmp/${SIM_NAME}.LC.disbatch
 
-# Now generate the checksum concatenation tasks
+    NEWLCDIR=$ABACUSSUMMIT_PERSIST/$SIM_SET/$SIM_NAME/lightcones.concat/
 
-LCEPI_SCRIPT=$(pwd)/rhea_post_process_lightcones_epilogue.sh
-cat >> $DISBATCH_TASKFILE << EOM
+    LCSCRIPT=$(pwd)/rhea_post_process_lightcones.py
+    echo "#DISBATCH PREFIX cd $LCDIR; $LCSCRIPT " > $DISBATCH_TASKFILE  # write
+    echo "#DISBATCH SUFFIX > $(pwd)/\${DISBATCH_NAMETASKS}_\${DISBATCH_JOBID}_\${DISBATCH_TASKID}.log 2>&1 " >> $DISBATCH_TASKFILE  # append
+
+    # Args could get dangerously long, use relative paths
+    (cd $LCDIR; find Step*/ -name 'LightCone*' | cut -d. -f1 | sort -u) >> $DISBATCH_TASKFILE  # Step0500/LightCone0_heal, etc
+
+    # Now generate the checksum concatenation tasks
+
+    LCEPI_SCRIPT=$(pwd)/rhea_post_process_lightcones_epilogue.sh
+    cat >> $DISBATCH_TASKFILE << EOM
 #DISBATCH BARRIER CHECK
 #DISBATCH PREFIX cd $NEWLCDIR/
 #DISBATCH SUFFIX ; $LCEPI_SCRIPT > $(pwd)/\${DISBATCH_NAMETASKS}_\${DISBATCH_JOBID}_\${DISBATCH_TASKID}.log 2>&1 
@@ -62,26 +65,27 @@ pid/
 rv/
 EOM
 
-cat >> $DISBATCH_TASKFILE << EOM
+    cat >> $DISBATCH_TASKFILE << EOM
 #DISBATCH BARRIER CHECK
 #DISBATCH PREFIX 
 #DISBATCH SUFFIX 
-rm -rf $LCDIR
-#DISBATCH BARRIER CHECK
+#rm -rf $LCDIR
+##DISBATCH BARRIER CHECK
 mv $NEWLCDIR $(dirname $LCDIR)/lightcones
 EOM
 
-# We can fill up to ~2000 nodes
-NNODES=16
-WALLTIME=1:00:00
+    # We can fill up to ~2000 nodes
+    NNODES=3
+    WALLTIME=1:00:00
 
-#mkdir -p ./logs/$SIM_NAME/disbatchLC
+    mkdir -p ./logs/$SIM_NAME/disbatchLC
 
-# 6 cores per task: only three processes per task, but cores have two hyperthreads.  So 5 tasks per node.
-#JOBID2=$(sbatch -t $WALLTIME -N $NNODES -c 6 \
-#        -o logs/$SIM_NAME/%x.out --mem=0 -A AST145 -p batch --parsable --job-name=${SIM_NAME}_PostProcessLC \
-#        --wrap "./disBatch/disBatch.py -e -p ./logs/$SIM_NAME/disbatchLC/$SIM_NAME $DISBATCH_TASKFILE && echo Lightcones completed successfully.")
-#EPILOG_DEPEND+=":$JOBID2"
+    # 4 cores per task: only two blosc threads per task, but cores have two hyperthreads.  So 8 tasks per node.
+    JOBID2=$(sbatch -t $WALLTIME -N $NNODES -c 4 \
+            -o logs/$SIM_NAME/%x.out --mem=0 -A AST145 -p batch --parsable --job-name=${SIM_NAME}_PostProcessLC \
+            --wrap "./disBatch/disBatch.py -e -p ./logs/$SIM_NAME/disbatchLC/$SIM_NAME $DISBATCH_TASKFILE && echo Lightcones completed successfully.")
+    EPILOG_DEPEND+=":$JOBID2"
+fi
 
 ##### Queue up the time slice processing #####
 
@@ -94,7 +98,7 @@ if [[ -n "$SLICEDIRS" ]]; then
     # Chunks should be specified with a literal pattern like slice1.000/*slab001?.L0_pack9.dat
     DISBATCH_TASKFILE=$(pwd)/tmp/${SIM_NAME}.timeslice.disbatch
 
-    TSSCRIPT=$(pwd)/rhea_post_process_timeslice.sh
+    TSSCRIPT=$(pwd)/rhea_post_process_timeslice.py
     echo "#DISBATCH PREFIX cd ${SIMDIR}; ${TSSCRIPT} \"" > $DISBATCH_TASKFILE  # write
     echo "#DISBATCH SUFFIX \"" >> $DISBATCH_TASKFILE  # append
 
@@ -125,10 +129,10 @@ if [[ -n "$SLICEDIRS" ]]; then
 $TSEPI_SCRIPT $SIMDIR/slices/z*
 EOM
 
-    cat >> $DISBATCH_TASKFILE << EOM
-#DISBATCH BARRIER CHECK
-rm -rf $SIMDIR/slice*.*
-EOM
+#    cat >> $DISBATCH_TASKFILE << EOM
+##DISBATCH BARRIER CHECK
+#rm -rf $SIMDIR/slice*.*
+#EOM
 
     # We can fill up to ??? nodes
     NNODES=3
@@ -136,7 +140,7 @@ EOM
 
     mkdir -p ./logs/$SIM_NAME/disbatchTS
 
-    JOBID3=$(sbatch -t $WALLTIME -N $NNODES -c 6 \
+    JOBID3=$(sbatch -t $WALLTIME -N $NNODES -c 4 \
             -o logs/$SIM_NAME/%x.out --mem=0 -A AST145 -p batch --parsable --job-name=${SIM_NAME}_PostProcessTS \
             --wrap "./disBatch/disBatch.py -e -p ./logs/$SIM_NAME/disbatchTS/$SIM_NAME $DISBATCH_TASKFILE && echo Time slices completed successfully.")
     EPILOG_DEPEND+=":$JOBID3"
