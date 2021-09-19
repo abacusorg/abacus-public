a558c1ea0089d90118faeac2a6b6941504a40555
Lehman Garrison <lgarrison@flatironinstitute.org>
2021-09-19 09:17:04 -0400

    Fix TNG ICs

--- Production/TNG300/slabify_ics.py
+++ Production/TNG300/slabify_ics.py
@@ -2,7 +2,8 @@
 '''Convert Illustris ICs to Abacus.
 
 Normally this would be a job for convert_particles_to_ic.py. But that plumbs the reading through ReadAbacus,
-which would need all sorts of special-casing for the conventions of Illustris HDF5 IC files.  So here we are...
+which would need all sorts of special-casing for the conventions of Illustris HDF5 IC files.  So we'll use
+this one-off script instead.
 '''
 
 import os
@@ -12,21 +13,26 @@ import h5py
 from Abacus import WriteAbacus
 
 h5fn = Path(os.environ['HOME']) / 'ceph/illustris/ic/TNG300-2/snap_ics.hdf5'
-cpd = 405
+cpd = 297
 outdir = h5fn.parent
 outformat = 'RVPID'
+
+for fn in outdir.glob('ic_*'):
+    fn.unlink()
 
 with h5py.File(h5fn, 'r') as h:
     pos = h['PartType1']['Coordinates'][:]
     vel = h['PartType1']['Velocities'][:]
     pids = h['PartType1']['ParticleIDs'][:]
     
-    boxsize = h['Header'].attrs['BoxSize']/1e3
+    boxsize = h['Header'].attrs['BoxSize']
     zinit = h['Header'].attrs['Redshift']
     
     print('Read the following parameters from header:')
     print(dict(h['Header'].attrs))
 
+boxsize *= 1e-3
+pos *= 1e-3
 # Convert from Gadget internal velocities to peculiar km/s
 vel *= (1+zinit)**-0.5
 
