39730ec4e59dd7f7dd15f273b750e09d2b3f1d66
Lehman Garrison <lgarrison@flatironinstitute.org>
2022-10-07 13:42:29 -0400

    Update Galilean test for aux12

--- Tests/Galilean/run_test.py
+++ Tests/Galilean/run_test.py
@@ -15,6 +15,8 @@ from Abacus import InputFile
 
 PID_MASK = np.uint64(0x7fff7fff7fff)
 
+aux12_t = np.dtype([('aux',np.uint64), ('aux2',np.uint32)], align=False)
+
 ci_dtype = np.dtype([('startindex',np.uint64),('startindex_with_ghost',np.uint64),
                     ('count',np.int32), ('active',np.int32),
                     ('mean_square_velocity', np.float32), ('max_component_velocity', np.float32),
@@ -40,12 +42,12 @@ def load_aux(params, state):
     lwd = Path(params['LocalWorkingDirectory'])
     auxfns = sorted(lwd.parent.glob(lwd.name + '*/read/aux*'))
     
-    aux = np.empty(state['np_with_ghost_state'], dtype=np.uint64)
+    aux = np.empty(state['np_with_ghost_state'], dtype=aux12_t)
     i = 0
     slabstart = np.empty(params['CPD']*params['NumZRanks']+1, dtype=np.uint64)
     slabstart[0] = 0
     for x,fn in enumerate(auxfns):
-        _aux = np.fromfile(fn, dtype=np.uint64)
+        _aux = np.fromfile(fn, dtype=aux12_t)
         aux[i:i+len(_aux)] = _aux
         i += len(_aux)
         slabstart[x+1] = i
@@ -59,11 +61,11 @@ def select_primaries(aux, slabstart, ci, state):
     cpd = state['cpd_state']
     ghost_radius = state['GhostRadius']
 
-    primaries = np.empty(NP, dtype=np.uint64)
+    primaries = np.empty(NP, dtype=aux12_t)
 
     i = 0
     for x in range(len(ci)):
-        primary_ci = ci[x].reshape(cpd, -1)[:,ghost_radius:-ghost_radius]
+        primary_ci = ci[x].reshape(cpd, -1)[:,ghost_radius:cpd-ghost_radius]
         for y in range(len(primary_ci)):
             rowstart = primary_ci[y,0]['startindex_with_ghost'] + slabstart[x]
             rowend = primary_ci[y,-1]['startindex_with_ghost'] + primary_ci[y,-1]['count'].astype(np.uint64) + slabstart[x]
@@ -91,7 +93,7 @@ def check_pid(pid, ppd):
 
 
 def main(nstep):
-    abacus.run('abacus.par2', maxsteps=nstep)
+    abacus.run('abacus.par2', maxsteps=nstep, clean=True)
 
     params = InputFile.InputFile('abacus.par')
     state = InputFile.InputFile(Path(params['WorkingDirectory'])/'read'/'state')
@@ -100,11 +102,15 @@ def main(nstep):
     aux, slabstart = load_aux(params, state)
     primaries = select_primaries(aux, slabstart, ci, state)
 
-    primary_pid = primaries & PID_MASK
+    primary_pid = primaries['aux'] & PID_MASK
     primary_pid.sort()
     ppd = int(round(state['ppd']))
 
-    print(check_pid(primary_pid, ppd))
+    nbad = check_pid(primary_pid, ppd)
+    if nbad == 0:
+        print('All primary PIDs present with no duplicates.')
+    else:
+        raise ValueError(f'Test failed; found {nbad} bad PIDs')
 
 
 if __name__ == '__main__':
